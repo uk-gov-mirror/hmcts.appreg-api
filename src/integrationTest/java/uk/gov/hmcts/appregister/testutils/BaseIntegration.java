54eaf6a0ffdf821a70f23577aee87f9647b5f9d2
Michael Estebanez <63051024+mestebanez@users.noreply.github.com>
2026-03-09 10:48:10 +0000

    Fix the hard coded schema in flyway script and add tests to prevent f… (#289)
    * Fix the hard coded schema in flyway script and add tests to prevent future regression
    
    * Ensure tests work
    
    * Ensure the flyway script aligns with MASTER
    
    * Fix security issues with run block

--- src/integrationTest/java/uk/gov/hmcts/appregister/testutils/BaseIntegration.java
+++ src/integrationTest/java/uk/gov/hmcts/appregister/testutils/BaseIntegration.java
@@ -41,7 +41,7 @@ public class BaseIntegration extends BasePostgresIntegrationTest {
     protected LogCaptor logCaptor;
 
     /** A data audit log asserter. */
-    protected DataAuditLogAsserter differenceLogAsserter;
+    @Autowired protected DataAuditLogAsserter differenceLogAsserter;
 
     /** An activity log asserter. */
     protected ActivityAuditLogAsserter activityAuditLogAsserter;
@@ -64,7 +64,6 @@ public class BaseIntegration extends BasePostgresIntegrationTest {
         }
 
         logCaptor = LogCaptor.forClass(AuditOperationSlf4jLogger.class);
-        differenceLogAsserter = new DataAuditLogAsserter();
         activityAuditLogAsserter = new ActivityAuditLogAsserter();
         logCaptor.clearLogs();
         differenceLogAsserter.clearLogs();
